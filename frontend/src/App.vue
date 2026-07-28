60ce008846ec7c06b45ed9275b3bc4d1870f5842
Azure <jc20abalajon@gmail.com>
2026-07-28 09:01:09 +0800

    Added port 5173 as an allowed origin in cors and sanctum; Created an api client

---
+++ frontend/src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+import { onMounted } from 'vue'
+import api from './api/axios'
+
+onMounted(() => {
+
+  api.get('/user')
+  .then(res => console.log('Success:', res.data))
+  .catch(err => console.log('Status:', err.response?.status, err.message))
+
+
+})
+
+</script>
+
+<template>
+  <p>Initial Commit</p>
+</template>
+
+<style scoped></style>
